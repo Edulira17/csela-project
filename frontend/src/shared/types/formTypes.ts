c0029cf5f163a5c4de4e4baab72c2916f1a4bdfd
Eduardo Lira <eduardoliracunha17@gmail.com>
2025-03-25 23:15:38 -0400

    feat: adiciona interfaces StudentTypes e GuardianTypes para tipagem de formulários

---
+++ frontend/src/shared/types/formTypes.ts
@@ -0,0 +1,16 @@
+export interface StudentTypes {
+  name: string;
+  naturalidade: string;
+  idade: number;
+  serie: number;
+  dataNascimento: Date;
+  turno: string;
+};
+
+export interface GuardianTypes {
+  name: string;
+  phone: number;
+  rg: string
+  cpf: string
+}
+
