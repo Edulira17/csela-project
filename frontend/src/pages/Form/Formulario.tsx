d372c26dee94abba684173f4f022e1d0f948ecbc
Eduardo Lira <eduardoliracunha17@gmail.com>
2025-03-28 23:11:29 -0400

    feat: adiciona seção de responsável ao formulário e implementa máscara nos campos

--- frontend/src/pages/Form/Formulario.tsx
+++ frontend/src/pages/Form/Formulario.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@mui/material";
 import { FormContainer,  ResponsiveContainerForm, ActionsContainer } from "./style";
 import StudentSection from "../../shared/components/FormSections/StudentSection";
+import ResponsibleSection from "../../shared/components/FormSections/ResponsibleSection";
 
 // add use-masked-input
 
@@ -8,9 +9,11 @@ const Formulario = () => {
   return (
     <ResponsiveContainerForm>
       <FormContainer>
+
         <h2>DADOS DOS ALUNOS</h2>
         <StudentSection />
         <h2>DADOS DO RESPONSÁVEL</h2>
+        <ResponsibleSection />
 
         <ActionsContainer>
           <Button size="medium" variant="contained" color="success" type="submit">Submit</Button>
--- frontend/src/shared/components/FormSections/ResponsibleSection/index.tsx
+++ frontend/src/shared/components/FormSections/ResponsibleSection/index.tsx
@@ -1,18 +1,21 @@
 import { useForm } from 'react-hook-form';
-import { FormGroup, Label, Input} from '../styles'
+import { FormGroup, Label, Input } from '../styles'
+import { useHookFormMask } from 'use-mask-input';
 
 const ResponsibleSection = () => {
-   const { register, formState: { errors } } = useForm();
+  const { register } = useForm();
+  const registerWithMask = useHookFormMask(register);
   return (
     <FormGroup>
       <Label>Nome do Responsável</Label>
       <Input type='text' {...register('name')} />
       <Label>Número para contato</Label>
-      <Input type='text' {...register('phone')}/>
+      <Input type='text' {...registerWithMask('phone', '(99) 99999-9999')} />
+      {/* customs inputs */}
       <Label>RG do Responsável</Label>
-      <Input type='text' {...register('rg')}/>
+      <Input type='text' {...registerWithMask('rg', '9999999-9')} />
       <Label>CPF do Responsável</Label>
-      <Input type='text' {...register('cpf')}/>
+      <Input type='text' {...registerWithMask('cpf', '999.999.999-99')} />
     </FormGroup>
   )
 };
