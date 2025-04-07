08849e43d98f4cf7df897524c3107b013a5cc7ab
Eduardo Lira <eduardoliracunha17@gmail.com>
2025-04-07 10:33:41 -0400

    feat: configura setup inicial para iniciar o servidor

---
+++ backend/src/server.ts
@@ -0,0 +1,19 @@
+import Fastify from "fastify";
+import { PrismaClient } from "@prisma/client";
+import routes from './routes/index'
+
+
+const app = Fastify();
+const prisma = new PrismaClient();
+
+app.register(routes, {prefix: '/api'})
+
+app.listen({port: 3333}, (err, address) => {
+  if(err){
+    console.error(err);
+    process.exit(1);
+  }
+
+  console.log(`Server is running at ${address}`)
+})
+
