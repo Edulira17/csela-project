a0b45016eedab1b6d97703451cf7c6122304c779
Eduardo Lira <eduardoliracunha17@gmail.com>
2025-04-11 10:43:56 -0400

    feat: tipa as propriedades do array de objetos projectsOffered

--- frontend/src/pages/Home/projectsOfferedData.ts
+++ frontend/src/pages/Home/projectsOfferedData.ts
@@ -1,22 +1,42 @@
+export interface ICardsProjects {
+  id: number;
+  title: string;
+  description: string;
+  dialogTitle: string;
+  dialogParagraphs: string[];
+}
 
-const projectsOffered = [
+
+const projectsOffered: ICardsProjects[] = [
   {
     id: 1,
     title: "ECAE (Espaço Cidadão de Arte e Educação)",
     description: "Oferece aulas de balé clássico, oficinas de leitura e jogos, leitura pelo computador, escola de música, biblioteca e espaço teatral.",
-    image: "./assets/offered_projects.svg",
+    dialogTitle: "ECAE (Espaço Cidadão de Arte e Educação)",
+    dialogParagraphs: [
+      "Oferece aulas de balé clássico, oficina de artes, espaço de leitura e jogos...",
+      "Leitura orientada, inclusão digital, escola de música, biblioteca e espaço literal"
+    ]
   },
   {
     id: 2,
     title: "Projeto Remo Vida - Atividades Esportivas",
-    description:  "Criado após um trágico acidente envolvendo cinco crianças que perderam a vida ao tentar atravessar o lago sem saber nadar.",
-    image: './assets/learning_swimming.svg',
+    description: "Criado após um trágico acidente envolvendo cinco crianças que perderam a vida ao tentar atravessar o lago sem saber nadar.",
+    dialogTitle: "ECAE (Espaço Cidadão de Arte e Educação)",
+    dialogParagraphs: [
+      "Oferece aulas de balé clássico, oficina de artes, espaço de leitura e jogos...",
+      "Leitura orientada, inclusão digital, escola de música, biblioteca e espaço literal"
+    ]
   },
   {
     id: 3,
     title: "Programa Água Para a Vida",
     description: "Atende famílias de diversas comunidades do bairro, promovendo o acesso à água potável e conscientização sobre sua importância.",
-    image: './assets/water_image.jpg',
+    dialogTitle: "ECAE (Espaço Cidadão de Arte e Educação)",
+    dialogParagraphs: [
+      "Oferece aulas de balé clássico, oficina de artes, espaço de leitura e jogos...",
+      "Leitura orientada, inclusão digital, escola de música, biblioteca e espaço literal"
+    ]
   }
 ];
 
