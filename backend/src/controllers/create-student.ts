d8fb1c8f87dd25b9d6f036b2ad7be66741fdc297
Eduardo Lira <eduardoliracunha17@hotmail.com>
2025-04-22 18:55:45 -0400

    refactor: ajusta controller para relacionamento one-to-one

--- backend/src/controllers/create-student.ts
+++ backend/src/controllers/create-student.ts
@@ -4,55 +4,70 @@ import { completeFormSchema } from "../schemas/registration-student-schema";
 
 const prisma = new PrismaClient();
 
-export async function createStudentHandler(req: FastifyRequest, reply: FastifyReply) {
+export async function createStudentHandler(
+  req: FastifyRequest,
+  reply: FastifyReply
+) {
   try {
-    const parsed = completeFormSchema.parse(req.body)
+    const parsed = completeFormSchema.parse(req.body);
 
     const student = await prisma.estudante.create({
       data: {
         nomeCompleto: parsed.studentInfo.nomeCompleto,
         naturalidade: parsed.studentInfo.naturalidade,
         idade: Number(parsed.studentInfo.idade),
-        dataNascimento: new Date(parsed.studentInfo.dataNascimento.split('/').reverse().join('-')),
+        dataNascimento: new Date(
+          parsed.studentInfo.dataNascimento.split("/").reverse().join("-")
+        ),
         escola: parsed.studentInfo.escola,
         serie: parsed.studentInfo.serie,
         turno: parsed.studentInfo.turno,
-
-        responsavel: {
-          create: {
-            nomeResponsavel: parsed.responsibleInfo.nomeResponsavel,
-            numeroContato: parsed.responsibleInfo.numeroContato,
-            rg: parsed.responsibleInfo.rg,
-            cpf: parsed.responsibleInfo.cpf,
-          }
+      },
+    });
+    await prisma.responsavel.create({
+      data: {
+        nomeResponsavel: parsed.responsibleInfo.nomeResponsavel,
+        numeroContato: parsed.responsibleInfo.numeroContato,
+        rg: parsed.responsibleInfo.rg,
+        cpf: parsed.responsibleInfo.cpf,
+        estudante: {
+          connect: { id: student.id },
         },
-
-        endereco: {
-          create: {
-            cep: parsed.addressInfo.cep,
-            rua: parsed.addressInfo.rua,
-            bairro: parsed.addressInfo.bairro,
-            cidade: parsed.addressInfo.cidade,
-          }
+      },
+    });
+    await prisma.endereco.create({
+      data: {
+        cep: parsed.addressInfo.cep,
+        rua: parsed.addressInfo.rua,
+        bairro: parsed.addressInfo.bairro,
+        cidade: parsed.addressInfo.cidade,
+        estudante: {
+          connect: { id: student.id },
         },
-
-        informacoesAdicionais: {
-          create: {
-            oficina: parsed.additionalInfo.oficina,
-            horario: parsed.additionalInfo.horario,
-            moradia: parsed.additionalInfo.moradia,
-            auxilio: parsed.additionalInfo.auxilio === "sim",
-            transporte: parsed.additionalInfo.transporte === "sim",
-            moradores: parsed.additionalInfo.moradores,
-            divulgacao: parsed.additionalInfo.divulgacao === "sim",
-          }
-        }
-      }
+      },
+    });
+    await prisma.informacoesAdicionais.create({
+      data: {
+        oficina: parsed.additionalInfo.oficina,
+        horario: parsed.additionalInfo.horario,
+        moradia: parsed.additionalInfo.moradia,
+        auxilio: parsed.additionalInfo.auxilio === "sim",
+        transporte: parsed.additionalInfo.transporte === "sim",
+        moradores: parsed.additionalInfo.moradores,
+        divulgacao: parsed.additionalInfo.divulgacao === "sim",
+        estudante: {
+          connect: { id: student.id },
+        },
+      },
     });
 
-    return reply.code(201).send({ message: 'Estudante cadastrado com sucesso' })
+    return reply
+      .code(201)
+      .send({ message: "Estudante cadastrado com sucesso" });
   } catch (error) {
-    console.error(error)
-    return reply.status(400).send({ error: 'Erro ao cadastrar estudante', details: error })
+    console.error(error);
+    return reply
+      .status(400)
+      .send({ error: "Erro ao cadastrar estudante", details: error });
   }
 }
