a33064a6e28d278f9819bcf1cab6294be238fad2
Eduardo Lira <eduardoliracunha17@hotmail.com>
2025-04-26 11:18:16 -0400

    feat: configura cors com permissões somente para a url do frontend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,16 +1,21 @@
 import Fastify from "fastify";
 import { PrismaClient } from "@prisma/client";
-import routes from './routes/create-student.route'
-
+import cors from "@fastify/cors";
+import routes from "./routes/create-student.route";
 
 const app = Fastify();
 const prisma = new PrismaClient();
 
-app.get('/', () => {
-  return "Server is running"
-})
+app.register(cors, {
+  origin: "http://localhost:5173/",
+  methods: ["POST", "GET"],
+  allowedHeaders: ["Content-Type"]
+});
 
-app.listen({port: 3333}).then(() => {
-  console.log('Server is running')
-})
+app.get("/", () => {
+  return "Server is running";
+});
 
+app.listen({ port: 3333 }).then(() => {
+  console.log("Server is running");
+});
