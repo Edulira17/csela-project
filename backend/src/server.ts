ac0436d2d19e9cf56073beadbe6dd2a10584552d
Eduardo Lira <eduardoliracunha17@hotmail.com>
2025-04-26 10:51:31 -0400

    refactor: altera estrutura da inicialização do servidor

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -6,14 +6,11 @@ import routes from './routes/create-student.route'
 const app = Fastify();
 const prisma = new PrismaClient();
 
-app.register(routes, {prefix: '/api'})
+app.get('/', () => {
+  return "Server is running"
+})
 
-app.listen({port: 3333}, (err, address) => {
-  if(err){
-    console.error(err);
-    process.exit(1);
-  }
-
-  console.log(`Server is running at ${address}`)
+app.listen({port: 3333}).then(() => {
+  console.log('Server is running')
 })
 
