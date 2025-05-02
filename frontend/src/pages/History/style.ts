912f6cfb445955ecf3ea1f3c0c9e343343f5db04
Eduardo Lira <eduardoliracunha17@hotmail.com>
2025-05-02 18:55:37 -0400

    refactor: simplifica a estrutura do componente History e ajusta estilos dos títulos

--- frontend/src/pages/History/style.ts
+++ frontend/src/pages/History/style.ts
@@ -9,46 +9,41 @@ export const HistoryContainer = styled.div`
   flex-direction: column;
   gap: 2rem;
 `
+interface TitleProps {
+  color?: string
+}
 
-export const TitleHistory = styled.h1`
-  font-size: 2rem;
+export const TitleHistory = styled.h1<TitleProps>`
+  font-size: 1.5rem;
   font-weight: 900;
-  color: #b22222;
-  
+  color: ${({color}) => color || "#b2222"};
+  text-align: center;
+  padding: 1rem;
 `
 
 export const HistoryContent = styled.div`
   max-width: 1280px;
   margin: 0 auto;
-  padding: 2rem;
-  
   display: flex;
   flex-direction: column;
   align-items: center;
+  padding: 1rem;
 
-  gap: 1rem;
+
   background-color: #fff;
   border-radius: 8px;
   box-shadow: 10px 10px 20px -10px rgba(0,0,0,0.75);
-
-  .text-history {
-    font-size: 1.2rem;
-  }
 `
+
+
 
 export const TextContainer = styled.div`
   max-width: 100%;
   text-align: justify;
   padding: 1.5rem;
-`
 
-export const TitleContainer = styled.div`
-  display: flex;
-  align-items: center;
-`
-
-export const Illustration = styled.img`
-  width: 90px;
-  height: auto;
-
+  @media screen and (max-width: 768px){
+    font-size: .8rem;
+    padding: .5rem;
+  }
 `
