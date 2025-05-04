849ecdff76f8009256d0cb4adc26b2f6ceed09bd
Eduardo Lira <eduardoliracunha17@hotmail.com>
2025-05-04 19:09:08 -0400

    refactor: reorganiza a estrutura do componente Workshops e ajusta estilos

--- frontend/src/pages/Workshops/Workshops.tsx
+++ frontend/src/pages/Workshops/Workshops.tsx
@@ -1,35 +1,47 @@
 import cardData from "./cardData";
 import {
-  CardContainer,
   Card,
   DescriptionImage,
   SubtitleImage,
   TitleImage,
   ResponsiveContainerWorkshops,
   AboutWorkshops,
+  SectionWrapper,
+  CardWrapper,
+  TextWrapper,
 } from "./styles";
 
 const Workshops = () => {
   return (
     <ResponsiveContainerWorkshops>
       <AboutWorkshops>
-        <h1 className="about-title">🛠 Oficinas que Transformam Vidas</h1>
-        <p className="about-text">O Centro Social e Educacional do Lago do Aleixo (CSELA) desenvolve uma ampla gama de oficinas com foco no fortalecimento educacional, cultural e social da comunidade local. As atividades são cuidadosamente planejadas para atender crianças, adolescentes e adultos, promovendo o acesso ao conhecimento, à arte e ao esporte como instrumentos de inclusão e transformação.</p>
+        <TextWrapper>
+          <h1 className="about-title">🛠 Oficinas que Transformam Vidas</h1>
+          <p className="about-text">
+            O Centro Social e Educacional do Lago do Aleixo (CSELA) desenvolve
+            uma ampla gama de oficinas com foco no fortalecimento educacional,
+            cultural e social da comunidade local. As atividades são
+            cuidadosamente planejadas para atender crianças, adolescentes e
+            adultos, promovendo o acesso ao conhecimento, à arte e ao esporte
+            como instrumentos de inclusão e transformação.
+          </p>
+        </TextWrapper>
       </AboutWorkshops>
       <hr />
-      <CardContainer>
-        {cardData.map((card) => (
-          <Card key={card.id}>
-            <img src={card.image} alt={card.title} />
-            <DescriptionImage>
-              <TitleImage>{card.title}</TitleImage>
-              <SubtitleImage>{card.subtitle}</SubtitleImage>
-            </DescriptionImage>
-          </Card>
-        ))}
-      </CardContainer>
+      <SectionWrapper>
+        <CardWrapper>
+          {cardData.map((card) => (
+            <Card key={card.id}>
+              <img src={card.image} alt={card.title} />
+              <DescriptionImage>
+                <TitleImage>{card.title}</TitleImage>
+                <SubtitleImage>{card.subtitle}</SubtitleImage>
+              </DescriptionImage>
+            </Card>
+          ))}
+        </CardWrapper>
+      </SectionWrapper>
     </ResponsiveContainerWorkshops>
-
   );
 };
 
--- frontend/src/pages/Workshops/styles.ts
+++ frontend/src/pages/Workshops/styles.ts
@@ -1,76 +1,86 @@
 import styled from "styled-components";
 
-
 export const ResponsiveContainerWorkshops = styled.div`
   min-height: 100vh;
   width: 100%;
   display: flex;
   flex-direction: column;
   justify-content: center;
-  gap: 1rem;
+  gap: 1.4rem;
   padding: 1rem;
-`
+`;
 
 export const AboutWorkshops = styled.section`
   background-color: #fff;
   padding: 2rem 3rem;
   border-radius: 1.5rem;
-  box-shadow:  0 4px 10px rgba(0, 0, 0, 0.1);
-  max-width: 80%;
+  box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);
+  max-width: 1400px;
   margin: 0 auto;
-
 
   .about-title {
     font-size: 2rem;
+    color: #b22222;
     font-weight: 700;
     text-align: center;
-    color: #1f2937;
     margin-bottom: 1.5rem;
-    @media (max-width: 600px) {
+    @media (max-width: 768px) {
       font-size: 1.5rem;
     }
   }
 
-  .about-text{
-    font-size: 1.125rem;
+  .about-text {
+    font-size: 1rem;
     line-height: 1.8;
-    color: #374151;
+    text-align: justify;
     @media (max-width: 600px) {
       font-size: 1rem;
     }
   }
 
-
-  @media (max-width: 600px){
+  @media (max-width: 600px) {
     padding: 2rem 1rem;
   }
+`;
+
+export const TextWrapper = styled.article`
+  max-width: 100%;
 `
-
-
-
-
 // Cards
-export const CardContainer = styled.section`
-  max-width: 1024px;
-  margin: 0 auto;
+export const SectionWrapper = styled.section`
+  width: 100%;
+  background-color: #fff;
+  padding: 2rem 0;
+  border-radius: 1.5rem;
+`;
 
-  display: flex;
-  flex-wrap: wrap;
+export const CardWrapper = styled.div`
+  max-width: 1080px; 
+  margin: 0 auto;
+  padding: 0 2rem;
+  display: grid;
+  grid-template-columns: repeat(3, minmax(250px, 1fr));
+  gap: 2rem;
   justify-content: center;
-  gap: 1rem;
-  
+
+  // 2 colunas para telas menores que 768px
+  @media screen and (max-width: 768px) {
+    grid-template-columns: repeat(2, minmax(250px, 1fr));
+  }
+
+  // 1 colunas para telas menores que 480px
+  @media screen and (max-width: 768px) {
+    grid-template-columns: 1fr;
+  }
 `;
 
 export const Card = styled.figure`
   display: flex;
   flex-direction: column;
-
-  width: 282px;
+  width: 100%;
   height: 362px;
-
   padding: 1rem;
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
-
   border-radius: 8px;
   background-color: white;
 
