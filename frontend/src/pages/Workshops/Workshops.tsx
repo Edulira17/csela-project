767a9ad964e08c5da11fb5e8034f83c35d4b6ac5
Eduardo Lira <eduardoliracunha17@gmail.com>
2025-05-06 19:13:11 -0400

    refactor: ajusta cards alterando cores e melhorando lógica da estilização

--- frontend/src/pages/Workshops/Workshops.tsx
+++ frontend/src/pages/Workshops/Workshops.tsx
@@ -1,47 +1,39 @@
 import cardData from "./cardData";
 import {
   Card,
-  DescriptionImage,
   SubtitleImage,
   TitleImage,
   ResponsiveContainerWorkshops,
   AboutWorkshops,
-  SectionWrapper,
-  CardWrapper,
-  TextWrapper,
+  CardWrapper
 } from "./styles";
 
 const Workshops = () => {
   return (
     <ResponsiveContainerWorkshops>
       <AboutWorkshops>
-        <TextWrapper>
-          <h1 className="about-title">🛠 Oficinas que Transformam Vidas</h1>
-          <p className="about-text">
-            O Centro Social e Educacional do Lago do Aleixo (CSELA) desenvolve
-            uma ampla gama de oficinas com foco no fortalecimento educacional,
-            cultural e social da comunidade local. As atividades são
-            cuidadosamente planejadas para atender crianças, adolescentes e
-            adultos, promovendo o acesso ao conhecimento, à arte e ao esporte
-            como instrumentos de inclusão e transformação.
-          </p>
-        </TextWrapper>
-      </AboutWorkshops>
-      <hr />
-      <SectionWrapper>
+        <h1 className="about-title">🛠 Oficinas que Transformam Vidas</h1>
+        <p className="about-text">
+          O Centro Social e Educacional do Lago do Aleixo (CSELA) desenvolve
+          uma ampla gama de oficinas com foco no fortalecimento educacional,
+          cultural e social da comunidade local. As atividades são
+          cuidadosamente planejadas para atender crianças, adolescentes e
+          adultos, promovendo o acesso ao conhecimento, à arte e ao esporte
+          como instrumentos de inclusão e transformação.
+        </p>
         <CardWrapper>
           {cardData.map((card) => (
             <Card key={card.id}>
               <img src={card.image} alt={card.title} />
-              <DescriptionImage>
+              <div className="card-description">
                 <TitleImage>{card.title}</TitleImage>
                 <SubtitleImage>{card.subtitle}</SubtitleImage>
-              </DescriptionImage>
+              </div>
             </Card>
           ))}
         </CardWrapper>
-      </SectionWrapper>
-    </ResponsiveContainerWorkshops>
+      </AboutWorkshops>
+    </ResponsiveContainerWorkshops >
   );
 };
 
--- frontend/src/pages/Workshops/styles.ts
+++ frontend/src/pages/Workshops/styles.ts
@@ -12,12 +12,16 @@ export const ResponsiveContainerWorkshops = styled.div`
 
 export const AboutWorkshops = styled.section`
   background-color: #fff;
-  padding: 2rem 3rem;
+  padding: 2.5rem;
   border-radius: 1.5rem;
   box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);
   max-width: 1400px;
   margin: 0 auto;
 
+  display: flex;
+  flex-direction: column;
+  gap: 1.8rem;
+  
   .about-title {
     font-size: 2rem;
     color: #b22222;
@@ -38,25 +42,10 @@ export const AboutWorkshops = styled.section`
     }
   }
 
-  @media (max-width: 600px) {
-    padding: 2rem 1rem;
-  }
 `;
 
-export const TextWrapper = styled.article`
-  max-width: 100%;
-`
 // Cards
-export const SectionWrapper = styled.section`
-  width: 100%;
-  background-color: #fff;
-  padding: 2rem 0;
-  border-radius: 1.5rem;
-`;
-
 export const CardWrapper = styled.div`
-  max-width: 1080px; 
-  margin: 0 auto;
   padding: 0 2rem;
   display: grid;
   grid-template-columns: repeat(3, minmax(250px, 1fr));
@@ -70,46 +59,63 @@ export const CardWrapper = styled.div`
 
   // 1 colunas para telas menores que 480px
   @media screen and (max-width: 768px) {
-    grid-template-columns: 1fr;
+    grid-template-columns: repeat(1, minmax(250px, 1fr));
   }
 `;
 
 export const Card = styled.figure`
   display: flex;
   flex-direction: column;
+  justify-content: center;
+  align-items: center;
   width: 100%;
-  height: 362px;
-  padding: 1rem;
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
   border-radius: 8px;
-  background-color: white;
+  background-color: #b22222;
+  padding: 1.5rem;
 
   img {
     width: 250px;
     height: 250px;
-
     object-fit: cover;
     border-radius: 8px;
   }
-`;
 
-export const DescriptionImage = styled.figcaption`
-  font-size: 0.7rem;
-  margin-top: 1rem;
-
-  display: flex;
-  flex-direction: column;
-  align-items: start;
-
-  max-width: 100%;
-  gap: 0.5rem;
+  .card-description {
+    max-width: 80%;  
+  }
 `;
 
 export const TitleImage = styled.h4`
-  font-weight: 900;
-  color: #000;
+  font-size: 1.2rem;
+  color: #fff;
+  font-weight: 700;
+  text-align: center;
+  margin-top: 1rem;
+  @media (max-width: 768px) {
+    font-size: 1rem;
+  }
 `;
 
 export const SubtitleImage = styled.p`
-  color: gray;
+  font-size: 0.8rem;
+  color: #fff;
+  text-align: center;
+  margin-top: 0.5rem;
+  @media (max-width: 768px) {
+    font-size: 0.7rem;
+  }
 `;
+
+// export const DescriptionImage = styled.figcaption`
+//   font-size: 0.7rem;
+//   margin-top: 1rem;
+
+//   display: flex;
+//   flex-direction: column;
+//   align-items: start;
+
+//   max-width: 100%;
+//   gap: 0.5rem;
+// `;
+
